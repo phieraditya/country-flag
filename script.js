490b7dce3dc3d38419413db7784b952801700015
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-02-15 16:16:26 +0700

    alert rejected promises

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ const renderCountry = function (data, className = '') {
 // Country 1
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
-    .then((response) => response.json())
+    .then(
+      (response) => response.json(),
+      (err) => alert(err)
+    )
     .then((data) => {
       renderCountry(data[0])
       const neighbour = data[0].borders[0]
@@ -37,8 +40,13 @@ const getCountryData = function (country) {
       // Country 2
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
     })
-    .then((response) => response.json())
+    .then(
+      (response) => response.json(),
+      (err) => alert(err)
+    )
     .then((data) => renderCountry(data, 'neighbour'))
 }
 
-getCountryData('indonesia')
+btn.addEventListener('click', function () {
+  getCountryData('cambodia')
+})
