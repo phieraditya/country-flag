fcbffcad50d57bb30c4ac6c637975e76536bacc1
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-02-22 15:17:41 +0700

    IIFE for returning value from async function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
-const btn = document.querySelector('.btn-country')
-const countriesContainer = document.querySelector('.countries')
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////////////////
 
@@ -19,23 +19,23 @@ const renderCountry = function (data, className = '') {
           <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
         </div>
       </article>
-  `
-  countriesContainer.insertAdjacentHTML('beforeend', html)
-  countriesContainer.style.opacity = 1
-}
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 const renderError = function (msg) {
-  countriesContainer.insertAdjacentHTML('beforeend', msg)
-  countriesContainer.style.opacity = 1
-}
+  countriesContainer.insertAdjacentHTML('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
 
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then((response) => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`)
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-    return response.json()
-  })
-}
+    return response.json();
+  });
+};
 
 ///////////////////////////////////////////////////
 
@@ -43,26 +43,26 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 const getCountryData = function (country) {
   getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
     .then((data) => {
-      renderCountry(data[0])
-      const neighbour = data[0].borders[0]
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
 
-      if (!neighbour) throw new Error('No neighbour found!')
+      if (!neighbour) throw new Error('No neighbour found!');
 
       // Country 2
       return getJSON(
         `https://restcountries.com/v2/alpha/${neighbour}`,
         'Country not found'
-      )
+      );
     })
     .then((data) => renderCountry(data, 'neighbour'))
     .catch((err) => {
-      console.error(`${err.message} 💥💥💥`)
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`)
-    })
+      console.error(`${err.message} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    });
   // .finally(() => {
   //   countriesContainer.style.opacity = 1
   // })
-}
+};
 
 // btn.addEventListener('click', function () {
 //   getCountryData('russia')
@@ -73,44 +73,50 @@ const getCountryData = function (country) {
 // Get Position
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject)
-  })
-}
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
 
 const whereAmI = async function () {
   try {
     // Geolocation
-    const pos = await getPosition()
-    const { latitude: lat, longitude: lng } = pos.coords
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
     // Reverse geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    if (!resGeo.ok) throw new Error(`Problem getting location data 💥💥`)
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error(`Problem getting location data 💥💥`);
 
-    const dataGeo = await resGeo.json()
+    const dataGeo = await resGeo.json();
 
     // Country data
     const response = await fetch(
       `https://restcountries.com/v2/name/${dataGeo.country}`
-    )
-    if (!response.ok) throw new Error(`Problem getting country 💥💥`)
+    );
+    if (!response.ok) throw new Error(`Problem getting country 💥💥`);
 
-    const data = await response.json()
-    renderCountry(data[0])
+    const data = await response.json();
+    renderCountry(data[0]);
 
-    return `You are in ${dataGeo.city}, ${dataGeo.country}`
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
   } catch (err) {
-    renderError(`${err.message}`)
+    renderError(`${err.message}`);
 
     // Reject promise returned from async function
-    throw err
+    throw err;
   }
-}
+};
 
 // btn.addEventListener('click', whereAmI)
 
-console.log('1: Will get location')
-whereAmI()
-  .then((city) => console.log(`2: ${city}`))
-  .catch((err) => console.error(`2: ${err.message}`))
-  .finally(() => console.log('3: Finished getting location'))
+console.log('1: Will get location');
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message}💥💥`);
+  }
+  console.log('3: Finished getting location');
+})();
